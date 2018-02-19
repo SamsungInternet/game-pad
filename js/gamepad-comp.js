e3a603a8aa3efb9653da82dd44a5439e312c847b
diekus <diekus@gmail.com>
2018-02-19 16:08:15 +0000

    basic connectivity gamepad

--- js/gamepad-comp.js
+++ js/gamepad-comp.js
@@ -1,7 +1,10 @@
+var gamepads = {};
+
 class GamepadWrapper extends HTMLElement {
 
     constructor(){
         super();
+
         var shadow = this.attachShadow({mode:'open'});
 
         var wrapper = document.createElement('div');
@@ -13,20 +16,51 @@ class GamepadWrapper extends HTMLElement {
         //styling for shadow dom
         var style = document.createElement('style');
         style.textContent = '.wrapper {'+
-                                'position: fixed;' +
-                                'top: 0px;' +
-                                'right: 0px;' +
-                                'height: 2em;' +
                                 'width: 2em;' +
-                                'border-radius: 50%;' +
                                 'padding: .5em;' +
-                                'background-color: khaki;' +
-                                'color: darkgoldenrod;' +  
+                                'color: white;' +  
                                 'transition: color .5s ease-in, background-color .5s ease-in;' +
+                                '}'+
+                                '.connected {'+
+                                'background-color: #00C800;'+
+                                '}'+
+                                '.not_connected {'+
+                                'background-color: #FFC800;'+
+                                '}'+
+                                '.not_supported {'+
+                                'background-color: #FF0000;'+
                                 '}';
         wrapper.appendChild(gpadicon);                        
         shadow.appendChild(style);
         shadow.appendChild(wrapper);
+
+        this.init_API();
+    }
+
+    init_API(){
+        if(navigator.getGamepads != undefined){
+            //attach events
+            window.addEventListener("gamepadconnected", function(e) { gamepadHandler(e, true); }, false);
+            window.addEventListener("gamepaddisconnected", function(e) { gamepadHandler(e, false); }, false);
+        }
+        else{
+            document.getElementsByTagName('game-pad')[0].shadowRoot.querySelector('.wrapper').classList.add('not_supported');
+        }
+    }
+}
+
+function gamepadHandler(event, connecting) {
+    var gamepad = event.gamepad;
+    if (connecting) {
+        gamepads[gamepad.index] = gamepad;
+        document.getElementsByTagName('game-pad')[0].shadowRoot.querySelector('.wrapper').classList.add('connected');
+        document.getElementsByTagName('game-pad')[0].shadowRoot.querySelector('.wrapper').classList.remove('not_connected');
+        console.log('connected ' + gamepad.id);
+    } else {
+        delete gamepads[gamepad.index];
+        document.getElementsByTagName('game-pad')[0].shadowRoot.querySelector('.wrapper').classList.add('not_connected');
+        document.getElementsByTagName('game-pad')[0].shadowRoot.querySelector('.wrapper').classList.remove('connected');
+        console.log('deleted ' + + gamepad.id);
     }
 }
 
